3abe672cda2a6b575d9a6a5a0aef64cb60831fe1
apschhokar <apschhokar@gmail.com>
2015-11-09 14:49:47 -0500

    Added Photo View Controller

---
+++ BarterApp/BarterAppController/TakeBookPhotoViewController.h
@@ -0,0 +1,18 @@
+//
+//  TakeBookPhotoViewController.h
+//  BarterApp
+//
+//  Created by ajay singh on 11/9/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TakeBookPhotoViewController : UIViewController <UIImagePickerControllerDelegate , UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)takePhoto:  (UIButton *)sender;
+
+
+@end
---
+++ BarterApp/BarterAppController/TakeBookPhotoViewController.m
@@ -0,0 +1,61 @@
+//
+//  TakeBookPhotoViewController.m
+//  BarterApp
+//
+//  Created by ajay singh on 11/9/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import "TakeBookPhotoViewController.h"
+
+@interface TakeBookPhotoViewController ()
+
+@end
+
+@implementation TakeBookPhotoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self openCamera];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+- (void)openCamera {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+    
+}
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.image = chosenImage;
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+
+
+@end
