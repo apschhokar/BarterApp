1ff1bdbaceffad57f4a06ad307b701f030f831a8
apschhokar <apschhokar@gmail.com>
2015-11-08 19:00:55 -0500

    Added books class and view controller

---
+++ BarterApp/BarterModel/Books.h
@@ -0,0 +1,13 @@
+//
+//  Books.h
+//  BarterApp
+//
+//  Created by ajay singh on 11/8/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Books : NSObject
+
+@end
---
+++ BarterApp/BarterModel/Books.m
@@ -0,0 +1,13 @@
+//
+//  Books.m
+//  BarterApp
+//
+//  Created by ajay singh on 11/8/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import "Books.h"
+
+@implementation Books
+
+@end
